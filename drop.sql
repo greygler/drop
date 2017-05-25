1c6491893595819d7d8d824aaf1e37548f42b1a7
GreyGler <igor.sayutin@gmail.com>
2017-05-25 12:35:51 +0300

    v25.05

---
+++ drop.sql
@@ -0,0 +1,113 @@
+-- phpMyAdmin SQL Dump
+-- version 3.5.1
+-- http://www.phpmyadmin.net
+--
+-- Хост: 127.0.0.1
+-- Время создания: Май 25 2017 г., 12:34
+-- Версия сервера: 5.5.25
+-- Версия PHP: 5.3.13
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- База данных: `drop`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `news`
+--
+
+CREATE TABLE IF NOT EXISTS `news` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `date` varchar(15) NOT NULL,
+  `subj` varchar(50) NOT NULL,
+  `text` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+
+--
+-- Дамп данных таблицы `news`
+--
+
+INSERT INTO `news` (`id`, `date`, `subj`, `text`) VALUES
+(1, '1495298658', '1 версия', 'Начались работы 1 версии ДРОП');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `order`
+--
+
+CREATE TABLE IF NOT EXISTS `order` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `order_id` int(11) NOT NULL,
+  `country` varchar(2) NOT NULL,
+  `bayer_name` varchar(100) NOT NULL,
+  `phone` varchar(20) NOT NULL,
+  `comment` varchar(256) NOT NULL,
+  `site` varchar(100) NOT NULL,
+  `ip` varchar(20) NOT NULL,
+  `delivery_adress` varchar(256) NOT NULL,
+  `utm_source` varchar(50) NOT NULL,
+  `utm_medium` varchar(50) NOT NULL,
+  `utm_term` varchar(50) NOT NULL,
+  `utm_content` varchar(50) NOT NULL,
+  `utm_campaign` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `users`
+--
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `registration` varchar(12) NOT NULL,
+  `users_group` tinyint(4) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `drop_key` varchar(50) NOT NULL,
+  `balance` decimal(6,2) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `users_group`
+--
+
+CREATE TABLE IF NOT EXISTS `users_group` (
+  `id_group` int(11) NOT NULL,
+  `name_group` varchar(50) NOT NULL,
+  `fa_logo` varchar(25) NOT NULL,
+  `fa_user` varchar(25) NOT NULL,
+  PRIMARY KEY (`id_group`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `users_group`
+--
+
+INSERT INTO `users_group` (`id_group`, `name_group`, `fa_logo`, `fa_user`) VALUES
+(0, 'Бан', 'fa-ban', 'fa-ban'),
+(1, 'Администратор', 'fa-trophy', 'fa-diamond'),
+(2, 'Управляющий', 'fa-line-chart', 'fa-star'),
+(5, 'Пользователь', 'fa-user-circle', 'fa-user');
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
