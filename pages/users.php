acfc6d043092096ffcada9fecfc1532a7a5e189d
GreyGler <igor.sayutin@gmail.com>
2017-05-25 21:12:30 +0300

    v25.05

--- pages/users.php
+++ pages/users.php
@@ -3,28 +3,35 @@
 </div>
 <ul class="pagination">
   <li class="disabled"><a href="#">&laquo;</a></li>
-  <li class="active"><a href="#">1 <span class="sr-only">(current)</span></a></li>
+  <li class="active"><a href="/?<?= http_build_query($_GET) ?>&begin=1">1 <span class="sr-only">(current)</span></a></li>
   <li><a href="#">2 <span class="sr-only">(current)</span></a></li>
   <li><a href="#">&laquo;</a></li>
-</ul> <select style="width: 70px; float: right;" class="form-control" size="1" name="pages">
-	<option selected value="5">5</option>
-	<option value="10">10</option>
+</ul><form role="form" class="page_form" action="<?= $_SERVER["PHP_SELF"] ?>"> 
+<? foreach($_GET as $key => $value) if ($key!="pages") echo('<input type="hidden" value="'.$value.'" name="'.$key.'">'); ?>
+<select id="view_pages1"  class="form-control" size="1" name="pages" onchange="this.form.submit()">
+<? foreach($view_pages as $key => $value) {?>
+	<option value="<?= $value ?>"><?= $value ?></option>
+	<? } ?>
+	<option selected value="all">Все</option>
 	</select>
+	
+	</form>
 
 <table class="table table-striped table-responsive" >
 	<tr valign="middle" class="info">
-		<td valign="middle"><small><strong>ID</small></strong></td>
-		<td valign="middle"><small><strong>Регистрация</small></strong></td>
-		<td valign="middle"><small><strong>Имя</small></strong></td>
-		<td valign="middle"><small><strong>Группа</small></strong></td>
-		<td valign="middle"><small><strong>Баланс</small></strong></td>
+		<td valign="middle"><small><strong>ID</strong></small></td>
+		<td valign="middle"><small><strong>Регистрация</strong></small></td>
+		<td valign="middle"><small><strong>Имя</strong></small></td>
+		<td valign="middle"><small><strong>Группа</strong></small></td>
+		<td valign="middle"><small><strong>Баланс</strong></small></td>
 		
 	</tr>
 <?
 $groups=autoring::groups();
 $count_users=db::cound_bd('users');
+$count_pages=ceil($count_users/$_GET['pages']);
 $result=db::connect_db(DB_HOST, DB_NAME, DB_LOGIN, DB_PASS);
- $result = mysql_query("SELECT * FROM users");
+$result = mysql_query("SELECT * FROM users LIMIT {$_GET['pages']}");
 $myrow = mysql_fetch_array($result);
 do
 { ?>
@@ -45,7 +52,17 @@ while ($myrow = mysql_fetch_array($result));
 </table>
 <ul class="pagination">
   <li class="disabled"><a href="#">&laquo;</a></li>
-  <li class="active"><a href="#">1 <span class="sr-only">(current)</span></a></li>
+  <li class="active"><a href="&begin=1">1 <span class="sr-only">(current)</span></a></li>
   <li><a href="#">2 <span class="sr-only">(current)</span></a></li>
   <li><a href="#">&laquo;</a></li>
 </ul>
+<form role="form" class="page_form" action="<?= $_SERVER["PHP_SELF"] ?>"> 
+<? foreach($_GET as $key => $value) if ($key!="pages") echo('<input type="hidden" value="'.$value.'" name="'.$key.'">'); ?>
+<select id="view_pages1"  class="form-control" size="1" name="pages" onchange="this.form.submit()">
+<? foreach($view_pages as $key => $value) {?>
+	<option value="<?= $value ?>"><?= $value ?></option>
+	<? } ?>
+	<option selected value="all">Все</option>
+	</select>
+	
+	</form>
