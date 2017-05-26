c71fc0cabef2712e42076f1a2e27ca059f049c11
GreyGler <igor.sayutin@gmail.com>
2017-05-26 14:13:05 +0300

    v26.05

--- pages/users.php
+++ pages/users.php
@@ -1,49 +1,74 @@
 <div class="page-header">
 		<h1>Пользователи системы</h1>
 </div>
+<? 
+function pagelink($params)
+{
+ $link='/?'.http_build_query($params);
+ return $link; 
+}
+
+$get_params=$_GET;
+if ($get_params['page_no']!="") $page_no=$get_params['page_no']; else $page_no=1; 
+$groups=autoring::groups();
+$count_users=db::cound_bd('users');
+if (($get_params['pages']!="") AND ($get_params['pages']!='all'))  $limit=$get_params['pages']; 
+else $limit=$view_pages['0'];
+$count_pages=ceil($count_users/$limit);
+?>
+
 <ul class="pagination">
-  <li class="disabled"><a href="#">&laquo;</a></li>
-  <li class="active"><a href="/?<?= http_build_query($_GET) ?>&begin=1">1 <span class="sr-only">(current)</span></a></li>
-  <li><a href="#">2 <span class="sr-only">(current)</span></a></li>
-  <li><a href="#">&laquo;</a></li>
-</ul><form role="form" class="page_form" action="<?= $_SERVER["PHP_SELF"] ?>"> 
-<? foreach($_GET as $key => $value) if ($key!="pages") echo('<input type="hidden" value="'.$value.'" name="'.$key.'">'); ?>
+  <li <? if ($page_no==1) {echo('class="disabled"'); $href="#"; } else {$get_params['page_no']=$page_no-1; $href=pagelink($get_params);} ?>><a href="<?= $href  ?>">&laquo;</a></li>
+  <? for ($i=1; $i<=$count_pages; $i++ ) { ?>
+  <li <? if ($i==$page_no) echo('class="active"') ?>><a href="<? $get_params['page_no']=$i; echo pagelink($get_params) ?>"><?= $i ?> <span class="sr-only">(current)</span></a></li>
+  <? } ?>
+  <!-- <li><a href="#">2 <span class="sr-only">(current)</span></a></li> -->
+  <li <? if ($page_no==$count_pages) { echo('class="disabled"'); $href="#";} else{ $get_params['page_no']=$page_no+1; $href=pagelink($get_params); }  ?>><a href="<?= $href  ?>">&raquo;</a></li>
+</ul>
+
+<form role="form" class="page_form" action="<?= $_SERVER["PHP_SELF"] ?>"> 
+<? foreach($get_params as $key => $value) if ($key!="pages") echo('<input type="hidden" value="'.$value.'" name="'.$key.'">'); ?>
 <select id="view_pages1"  class="form-control" size="1" name="pages" onchange="this.form.submit()">
 <? foreach($view_pages as $key => $value) {?>
-	<option value="<?= $value ?>"><?= $value ?></option>
+	<option <? if ($limit==$value) echo ("selected"); ?> value="<?= $value ?>"><?= $value ?></option>
 	<? } ?>
-	<option selected value="all">Все</option>
+	<option value="all">Все</option>
 	</select>
 	
 	</form>
 
 <table class="table table-striped table-responsive" >
 	<tr valign="middle" class="info">
-		<td valign="middle"><small><strong>ID</strong></small></td>
-		<td valign="middle"><small><strong>Регистрация</strong></small></td>
-		<td valign="middle"><small><strong>Имя</strong></small></td>
-		<td valign="middle"><small><strong>Группа</strong></small></td>
-		<td valign="middle"><small><strong>Баланс</strong></small></td>
-		
+		<td valign="middle"><strong>ID</strong></td>
+		<td valign="middle"><strong>Имя</strong></td>
+		<td valign="middle"><strong>Группа</strong></td>
+		<td valign="middle"><strong>Баланс</strong></td>
+		<td valign="middle"><strong>Регистрация</strong></td>
 	</tr>
 <?
-$groups=autoring::groups();
-$count_users=db::cound_bd('users');
-if ($_GET['pages']!="") $count_pages=ceil($count_users/$_GET['pages']);
-else $count_pages=$count_users;
+
+
 $result=db::connect_db(DB_HOST, DB_NAME, DB_LOGIN, DB_PASS);
-//$result = mysql_query("SELECT * FROM users LIMIT {$_GET['pages']}");
-$result = mysql_query("SELECT * FROM users");
+$result = mysql_query("SELECT * FROM users LIMIT {$limit}");
+
 $myrow = mysql_fetch_array($result);
 do
 { ?>
 	<tr valign="middle" <? if ($myrow['users_group']==0) echo('class="danger"') ?> >
-		<td valign="middle"><small><?= $myrow['id'] ?></small></td>
-		<td valign="middle"><small><?= date("d.m.Y G:i",$myrow['registration']) ?></small></td>
-		<td valign="middle"><small><?= $myrow['name'] ?></small></td>
-		<td valign="middle"><small><span class="fa <?= $groups[$myrow['users_group']]['fa_user'] ?> fa-lg"></span> <?= $groups[$myrow['users_group']]['name_group']; ?></small></td>
-		<td valign="middle"><small><?= $myrow['balance'] ?> <?= CURRENCY ?></small></td>
+		<td valign="middle"><?= $myrow['id'] ?></td>
 		
+		<td valign="middle"><?= $myrow['name'] ?></td>
+		<td valign="middle"> <!--<span class="fa <?= $groups[$myrow['users_group']]['fa_user'] ?> fa-lg"></span> -->
+		<select id="view_pages1"  class="form-control" size="1" name="pages" onchange="">
+		<? foreach ($groups as $key => $value)  { ?>
+		<option <? if ($key==$myrow['users_group']) echo ("selected") ; ?> value="<?= $key ?>"><?= $value['name_group'] ?></option>
+		<!--<option value="<?= $myrow['users_group'] ?>"><?= $groups[$myrow['users_group']]['name_group']; ?></option> -->
+		<? } ?>
+		</select>
+		
+		</td>
+		<td valign="middle"><?= $myrow['balance'] ?> <?= CURRENCY ?></td>
+		<td valign="middle"><?= date("d.m.Y G:i",$myrow['registration']) ?></td>
 	</tr>
 <?	
 //echo $myrow['ИМЯ_ПОЛЯ1'];
@@ -52,19 +77,23 @@ do
 while ($myrow = mysql_fetch_array($result));
 ?>
 </table>
+
 <ul class="pagination">
-  <li class="disabled"><a href="#">&laquo;</a></li>
-  <li class="active"><a href="&begin=1">1 <span class="sr-only">(current)</span></a></li>
-  <li><a href="#">2 <span class="sr-only">(current)</span></a></li>
-  <li><a href="#">&laquo;</a></li>
+  <li <? if ($page_no==1) {echo('class="disabled"'); $href="#"; } else {$get_params['page_no']=$page_no-1; $href=pagelink($get_params);} ?>><a href="<?= $href  ?>">&laquo;</a></li>
+  <? for ($i=1; $i<=$count_pages; $i++ ) { ?>
+  <li <? if ($i==$page_no) echo('class="active"') ?>><a href="<? $get_params['page_no']=$i; echo pagelink($get_params) ?>"><?= $i ?> <span class="sr-only">(current)</span></a></li>
+  <? } ?>
+  <!-- <li><a href="#">2 <span class="sr-only">(current)</span></a></li> -->
+  <li <? if ($page_no==$count_pages) { echo('class="disabled"'); $href="#";} else{ $get_params['page_no']=$page_no+1; $href=pagelink($get_params); }  ?>><a href="<?= $href  ?>">&raquo;</a></li>
 </ul>
+
 <form role="form" class="page_form" action="<?= $_SERVER["PHP_SELF"] ?>"> 
-<? foreach($_GET as $key => $value) if ($key!="pages") echo('<input type="hidden" value="'.$value.'" name="'.$key.'">'); ?>
+<? foreach($get_params as $key => $value) if ($key!="pages") echo('<input type="hidden" value="'.$value.'" name="'.$key.'">'); ?>
 <select id="view_pages1"  class="form-control" size="1" name="pages" onchange="this.form.submit()">
 <? foreach($view_pages as $key => $value) {?>
-	<option value="<?= $value ?>"><?= $value ?></option>
+	<option <? if ($limit==$value) echo ("selected"); ?> value="<?= $value ?>"><?= $value ?></option>
 	<? } ?>
-	<option selected value="all">Все</option>
+	<option value="all">Все</option>
 	</select>
 	
 	</form>
